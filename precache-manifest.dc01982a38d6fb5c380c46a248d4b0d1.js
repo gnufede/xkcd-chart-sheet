5821f69d0034780e0e418b5a7ca3ab45b657553b
Fede <gnufede@gmail.com>
2019-08-27 16:50:26 +0200

    Updates

---
+++ precache-manifest.dc01982a38d6fb5c380c46a248d4b0d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b516f33bc4f6722fea0b02b601b56e6",
+    "url": "/xkcd-chart-sheet/index.html"
+  },
+  {
+    "revision": "35981c474c59430b7684",
+    "url": "/xkcd-chart-sheet/static/css/main.eab2ca4b.chunk.css"
+  },
+  {
+    "revision": "958183fb289190ce8886",
+    "url": "/xkcd-chart-sheet/static/js/2.8aa30d8c.chunk.js"
+  },
+  {
+    "revision": "35981c474c59430b7684",
+    "url": "/xkcd-chart-sheet/static/js/main.b2215bf7.chunk.js"
+  },
+  {
+    "revision": "35f5b808a22b609d810e",
+    "url": "/xkcd-chart-sheet/static/js/runtime~main.d473a84e.js"
+  }
+]);
